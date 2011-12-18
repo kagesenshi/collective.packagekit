019191c1678cd782eed977933f0a0082dc0b0535
Izhar Firdaus <izhar@inigo-tech.com>
2011-12-19 01:28:22 +0800

    initial import

---
+++ collective/packagekit/tests.py
@@ -0,0 +1,62 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import collective.packagekit
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['collective.packagekit'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              collective.packagekit)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='collective.packagekit',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='collective.packagekit.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt', 
+            package='collective.packagekit',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        #ztc.FunctionalDocFileSuite(
+        #    'BROWSER.TXT',
+        #    package='collective.packagekit',
+        #    optionflags = OPTION_FLAGS,
+        #    test_class=TestCase),
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
