0812a9b562c343a95f02f2b05903bbbba0608a2a
Izhar Firdaus <izhar@inigo-tech.com>
2011-12-23 16:50:15 +0800

    update xcatalog_view for datagridfield

--- collective/packagekit/browser/xcatalog_view.py
+++ collective/packagekit/browser/xcatalog_view.py
@@ -7,11 +7,19 @@ class XCatalogView(grok.View):
 
     def render(self):
         self.request.response.setHeader('Content-type', 'application/x-catalog')
-        result =  '''
-        [PackageKit Catalog]
 
-        InstallPackages(fedora)=%s
-        ''' % ';'.join(self.context.pk_packages)
+        items = {}
+        for pkg in self.context.pk_packages:
+            distid = pkg['distribution']
+            if pkg['release']:
+                distid = '%s;%s' % (distid, pkg['release'])
+            items.setdefault(distid, [])
+            items[distid].append(pkg['identifier'])
+
+        result =  '[PackageKit Catalog]\n'
+
+        for key, value in items.items():
+            result += '\nInstallPackages(%s)=%s\n' % (key, ';'.join(value))
 
         self.request.response.setHeader('Content-Length',len(result))
         self.request.response.setHeader('Content-disposition', 
